a3f0bebe84240bea54c36bc0a97a01049db56309
Po Man <pman@prodessebv.nl>
2023-11-26 15:29:40 +0100

    Fix: Cleaned the code in app.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,48 +1,45 @@
-<!--<template>-->
-<!--  <img alt="Vue logo" src="./assets/logo.png">-->
-<!--  <HelloWorld></Helloworld>-->
-<!--</template>-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld></Helloworld>
-    <button @click="openModal">Open</button>
-    <DemoModal v-show="isModalVisible" @close="closeModal" />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld></Helloworld>
+        <button @click="openModal">Open</button>
+        <DemoModal v-show="isModalVisible" @close="closeModal"/>
+    </div>
 </template>
 <script>
-  import HelloWorld from './components/HelloWorld.vue';
-  import DemoModal from './components/anno_edit/DemoModal.vue';
-  export default {
-    name: 'App',
-    components: {
-      DemoModal,
-      HelloWorld,
-    },
-    data() {
-      return {
-        isModalVisible: false,
-      };
-    },
-    methods: {
-      openModal() {
-        this.isModalVisible = true;
-      },
-      closeModal() {
-        this.isModalVisible = false;
-      }
-    }
-  };
+    import HelloWorld from './components/HelloWorld.vue';
+    import DemoModal from './components/anno_edit/DemoModal.vue';
+
+    export default {
+        name: 'App',
+        components: {
+            DemoModal,
+            HelloWorld,
+        },
+        data() {
+            return {
+                isModalVisible: false,
+            };
+        },
+        methods: {
+            openModal() {
+                this.isModalVisible = true;
+            },
+            closeModal() {
+                this.isModalVisible = false;
+            }
+        }
+    };
 </script>
 
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
