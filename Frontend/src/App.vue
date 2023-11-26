4b114fe1ab9654c6a4a4b20eb79eae3d9254d83d
Po Man <pman@prodessebv.nl>
2023-11-26 14:26:11 +0100

    Feature: Added the page to edit the xml file

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,18 +1,40 @@
+<!--<template>-->
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld></Helloworld>-->
+<!--</template>-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld></Helloworld>
+    <button @click="openModal">Open</button>
+    <DemoModal v-show="isModalVisible" @close="closeModal" />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import HelloWorld from './components/HelloWorld.vue';
+  import DemoModal from './components/anno_edit/DemoModal.vue';
+  export default {
+    name: 'App',
+    components: {
+      DemoModal,
+      HelloWorld,
+    },
+    data() {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      openModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    }
+  };
 </script>
+
 
 <style>
 #app {
