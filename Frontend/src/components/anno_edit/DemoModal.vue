6b27bb622a40b30f485ebf0c61a4fa9c1e34b671
Hooshang <112929657+HooshB@users.noreply.github.com>
2024-01-21 15:42:40 +0100

    feat: Styling DemoModal

--- Frontend/src/components/anno_edit/DemoModal.vue
+++ Frontend/src/components/anno_edit/DemoModal.vue
@@ -387,13 +387,37 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.modal-container,
+.modal-container h1,
+.modal-container h3,
+.modal-container label,
+.modal-container .save-message,
+.modal-container .submit-btn,
+.modal-container .close-btn {
+  color: #ffffff;
+}
+
+button {
+  height: 50px;
+  width: 175px;
+  border-radius: 15px;
+  border: 2px solid #cccccc;
+  background-color: #596db8;
+  color: #ffffff;
+  font-size: 20px;
+}
+
+.close-btn {
+  background-color: #ff0404fa;
+  color: #ffffff;
+  font-size: 20px;
+}
 .modal-container {
-  width: 70%; /* Increase the width of the modal */
+  width: 70%; 
   margin: auto; 
   z-index: 1;
 }
-
 .row {
   align-items: center;
 }
